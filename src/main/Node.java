2e8c9b9864deef9a32930cad0cc740c9fd7bdaf5
209489 <lukasztracz195@gmail.com>
2019-03-31 17:28:08 +0200

    Created class Node

---
+++ src/main/Node.java
@@ -0,0 +1,68 @@
+package main;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@ToString
+@Setter
+public class Node implements Comparable<Node> {
+    private int indexCity;
+    private Node parent;
+    private double valueOfHeuristic;
+    private List<Integer> way;
+    private double cost;
+    public static int numbersOfNodes = 0;
+
+
+    public Node(Node parent, double cost, int indexCity) {
+
+        if(numbersOfNodes == 0){
+            this.parent = null;
+            this.cost = 0.0;
+            way = new ArrayList<>();
+            way.add(indexCity);
+
+        }else{
+            this.parent = parent;
+            this.cost = parent.getCost() + cost;
+            way = new ArrayList<>(parent.getWay());
+            way.add(indexCity);
+
+        }
+        valueOfHeuristic = Double.NaN;
+
+        this.indexCity = indexCity;
+        numbersOfNodes++;
+    }
+
+    public Node(Node parent, double cost, int indexCity, double valueOfHeuristic) {
+
+        if(numbersOfNodes == 0){
+            this.parent = null;
+            this.cost = 0.0;
+            way = new ArrayList<>();
+            way.add(indexCity);
+            valueOfHeuristic = 0.0;
+
+        }else{
+            this.parent = parent;
+            this.cost = parent.getCost() + cost;
+            way = new ArrayList<>(parent.getWay());
+            way.add(indexCity);
+            this.valueOfHeuristic = valueOfHeuristic;
+
+        }
+
+        this.indexCity = indexCity;
+        numbersOfNodes++;
+    }
+
+    @Override
+    public int compareTo(Node node) {
+        return Double.compare(this.cost, node.getCost());
+    }
+}
