e2dbc7e6e9a807e60820540ad394558181bcb0e2
209489 <lukasztracz195@gmail.com>
2019-03-31 14:27:30 +0200

    Created test for class Matrix

---
+++ test/javaTest/MatrixTest.java
@@ -0,0 +1,41 @@
+package javaTest;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.SortedMap;
+
+import static org.junit.Assert.assertEquals;
+
+
+public class MatrixTest {
+    private List<City> listOfCity;
+    private Matrix matrix;
+
+    @BeforeClass
+    void initMatrix() {
+        listOfCity = new ArrayList<>();
+        for (int row = 0; row < 2; row++) {
+            for (int col = 0; col < 2; col++) {
+                listOfCity.add(new City(row, col));
+            }
+        }
+        matrix = new Matrix(listOfCity, 0);
+    }
+
+    @Test
+    public void getMapColumn() {
+        SortedMap<Double, Integer> column = matrix.getMapColumn(2);
+        Integer numberCityWithMinValue = column.get(column.firstKey());
+        assertEquals(numberCityWithMinValue, 3, 0.0);
+    }
+    @Test
+    public void getDistance(){
+       assertEquals( matrix.getDistance(1,1),0.0,0.0);
+       assertEquals(matrix.getDistance(1,3),1.0,0.0);
+    }
+
+
+}
